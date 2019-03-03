7f93924ee01c076a10ae1d983efab519ca250c00
Mateus Schroeder da Silva <mateus@debian>
2019-03-02 22:24:35 -0300

    updating program names to english

---
+++ programs/primes/primes.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N_MAX_PRIMOS 1000000000000000000 // 18 zeros + 1 , 19 dígitos
+
+int main()
+{
+    // Variaveis
+    int primos[N_MAX_PRIMOS] = {};
+
+    /* Corpo */
+
+    // Verificar se é primo
+    /*
+    for(i=0; i < N_MAX_PRIMOS + 1 ; i++)
+    {
+        if( n % primos[i] != 0){
+            // 'n' não é divisível pelo primo[i]
+            ch_primo = 'f'
+        }
+    }
+    */
+
+    if (1 % 0 == 10) puts("FILHA DA PUTA");
+
+    return 0;
+}
---
+++ programs/sequences/p-serie/p.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Variaveis
+    //
+    // Corpo
+    //
+    //
+
+    prinft("Digite 'p': ");
+    prinft("Valor máximo para 'n': ");
+    printf();
+
+
+    return 0;
+}
